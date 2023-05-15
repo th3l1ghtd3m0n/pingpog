3373993860f7ec090843676b550d52e6c05c399b
Lucas Rodrigues <th3l1ghtd3m0n@gmail.com>
2023-05-15 11:31:49 -0300

    feat(pingpog.asm): fill screen with solid color

--- pingpog.asm
+++ pingpog.asm
@@ -1,4 +1,18 @@
+    mov ah, 0x00
+    mov al, 0x13
+    int 0x10
+
+    mov ax, 0xA000
+    mov es, ax
+
+    mov bx, 0
 loop:
-    jmp loop
+    mov BYTE [es:bx], 0x0B
+    inc bx
+    cmp bx, 64000
+    jb loop
+
+    jmp $
+
     times 510 - ($-$$) db 0
-    dw 0xa55
+    dw 0xaa55
